16d8ccc00ebdef883336be31497a6651a5bdb61d
Parth Sareen <psareen@uwaterloo.ca>
2024-06-13 17:51:51 -0400

    wip (#3)
    * wip
    
    * Wip
    
    * Fix function node
    
    * Updates to UX

--- dagent/src/dagent/FunctionNode.py
+++ dagent/src/dagent/FunctionNode.py
@@ -1,14 +1,18 @@
 from .DAG_Node import DAG_Node
-from typing import Callable, Any, Dict, Optional
 
 class FunctionNode(DAG_Node):
-    def __init__(self, func: Callable, next_nodes: Dict[str, DAG_Node] = None):
+    def __init__(self, func: callable, tool_description = dict | None, next_nodes: dict[str, DAG_Node] = None, user_params: dict | None = None):
         super().__init__(func, next_nodes)
+        self.tool_description = tool_description
+        self.user_params = user_params or {}
 
-    def run(self, *args, **kwargs) -> Any:
-        # Run the function and store the result
-        self.node_result = self.func(*args, **kwargs)
+    def run(self, **kwargs) -> any:
+        self.node_result = self.func(**kwargs)
         # Pass the result to the next nodes if any
+        # TODO: figure out param logic pattern
         for _, next_node in self.next_nodes.items():
-            next_node.run(self.node_result, **(kwargs or {}))
-
+            # TODO: add pydantic validation + cover in compile method
+            # 
+            params = {'prev_output': self.node_result, **next_node.user_params}
+            next_node.run(**params)
+            # next_node.run(**)
